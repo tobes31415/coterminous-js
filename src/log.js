7db46d9f8a0f4d73020ff863126106ba24e28872
Jake Tober <jake@macadamian.com>
2016-07-26 00:18:45 -0400

    log works on browser and on node js avoid duplicate bower.json file in bower

--- src/log.js
+++ src/log.js
@@ -1,11 +1,29 @@
 export default function logger(name)
 {
     var prefix = `[${name}]`;
-    return {
-        debug: window.console.debug.bind(window.console, prefix),
-        warn: window.console.warn.bind(window.console, prefix),
-        error: window.console.error.bind(window.console, prefix),
-        info: window.console.info.bind(window.console, prefix),
-        trace: window.console.trace.bind(window.console, prefix)
+    
+    var isBrowser = false;
+    try{isBrowser = window && window.console;}
+    catch(ignored){}
+
+    if (isBrowser)
+    {
+        return {
+            debug: window.console.debug.bind(window.console, prefix),
+            warn: window.console.warn.bind(window.console, prefix),
+            error: window.console.error.bind(window.console, prefix),
+            info: window.console.info.bind(window.console, prefix),
+            trace: window.console.trace.bind(window.console, prefix)
+        }
+    }
+    else
+    {
+        return {
+            debug: console.log.bind(console, '[DEBUG]'+prefix),
+            warn: console.log.bind(console, '[WARN]'+prefix),
+            error: console.log.bind(console, '[ERROR]'+prefix),
+            info: console.log.bind(console, '[INFO]'+prefix),
+            trace: console.log.bind(console, '[TRACE]'+prefix),
+        }
     }
 }
