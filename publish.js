c06243bab8eb4529b9a0a198a3eb0e45a5759613
Jake Tober <jake@macadamian.com>
2016-08-01 18:16:34 -0400

    Adding new capabilities to publish.js

--- publish.js
+++ publish.js
@@ -1,4 +1,8 @@
 import Coterminous from './src/coterminous.js';
 import * as handshaker from './src/handshaker.js';
 import * as rootInterface from './src/rootInterface.js';
+import * as functionPassing from './src/functionPassing.js';
+import * as promisePassing from './src/promisePassing.js';
+import * as datePassing from './src/datePassing.js';
+import * as regexPassing from './src/regexPassing.js';
 export default Coterminous;
