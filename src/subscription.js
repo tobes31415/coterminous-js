47b8e7f5f37ee272f8dbbbcaba3cef6ae7d260fc
Jake Tober <jake@macadamian.com>
2016-09-12 15:48:07 -0400

    Changing syntax of Subscription class

--- src/subscription.js
+++ src/subscription.js
@@ -1,7 +1,7 @@
 import logger from './log.js';
 var log = logger("Subscription");
 
-export default class
+class Subscription
 {
     constructor ()
     {
@@ -35,3 +35,5 @@ export default class
         };
     }
 }
+
+export default (Subscription);
