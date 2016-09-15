1421717bd781eb1fb09291f61e1051c063d3dfb2
Christian Nadeau <cnadeau@macadamian.com>
2016-09-14 21:48:27 -0400

    WIP adding coterminous-js-loopback as part of the unit tests
    Tried to get istanbul to properly run (runs, still not coverage) : https://github.com/gotwarlost/istanbul/issues/538#issuecomment-184446128

--- test/coterminous-spec.js
+++ test/coterminous-spec.js
@@ -1,8 +1,12 @@
 import assert from 'assert';
+
 import {
   registerCapability
 }
 from '../src/coterminous.js';
+
+import * as Coterminous from '../src/coterminous.js';
+import * as Loopback from 'coterminous-js-loopback';
 
 var mockedCapability = {
   "name": "mocked",
@@ -16,17 +20,42 @@ var mockedCapability = {
     Message,
     Cache
   }) {}
-}
-describe('Coterminous', function() {
-  it('registerCapability invalid capability', function() {
-    assert.throws(() => {
-      registerCapability({})
-    }, 'Should have thrown');
+};
+
+describe('Coterminous', () => {
+
+  describe('registerCapability', () => {
+
+    it('invalid capability', () => {
+      assert.throws(() => {
+        registerCapability({})
+      }, 'Should have thrown');
+    });
+
+    it('valid capability', () => {
+      assert.doesNotThrow(() => {
+        registerCapability(mockedCapability)
+      }, 'Should not have thrown');
+    });
+
   });
 
-  it('registerCapability valid capability', function() {
-    assert.doesNotThrow(() => {
-      registerCapability(mockedCapability)
-    }, 'Should not have thrown');
+  describe('connect test', () => {
+
+    it('ensure root definition', () => {
+      Coterminous.root({
+        hello: function() {
+          return "world";
+        }
+      });
+
+      var transport = new Loopback();
+
+      Coterminous.connect(transport).then(function(remoteInterface) {
+        assert.equal(remoteInterface.hello(), 'world', 'Root remote interface should have been used');
+      });
+    });
   });
+
+
 });
---
+++ test/index.js
@@ -0,0 +1,6 @@
+global = global || {};
+global.enableCoterminusLogs = true;
+
+// Load test suites
+require('./coterminous-spec.js');
+require('./walkObject-spec.js');
